f7cad40d3d4c4de471ba6547f4d6fa2714a01b7c
TrippyVoid29 <perczynski@student.agh.edu.pl>
2024-09-13 10:54:31 +0200

    changed vga_pkg values

--- rtl/vga_files/vga_pkg.sv
+++ rtl/vga_files/vga_pkg.sv
@@ -10,16 +10,21 @@
  package vga_pkg;
 
     // Parameters for VGA Display 800 x 600 @ 60fps using a 40 MHz clock;
-    localparam HOR_PIXELS = 800;
-    localparam VER_PIXELS = 600;
-    localparam HOR_BLANK_START = 800;
-    localparam VER_BLANK_START = 600;
-    localparam HOR_SYNC_START = 840;
-    localparam VER_SYNC_START = 601;
-    localparam VER_SYNC_STOP = 605;
-    localparam H_MAX = 1056;
-    localparam V_MAX = 628;
-    localparam HOR_SYNC_STOP = 968;
+    localparam HOR_PIXELS = 1024;
+    localparam VER_PIXELS = 768;
+
+    localparam H_MAX = 1344;
+    localparam HOR_BLANK_START = 1024;
+    localparam HOR_SYNC_START = 1048;
+    localparam HOR_SYNC_STOP = 1184;
+
+    localparam V_MAX = 806;
+    localparam VER_BLANK_START = 768;
+    localparam VER_SYNC_START = 771;
+    localparam VER_SYNC_STOP = 777;
+    
+    
+    
     
     // Add VGA timing parameters here and refer to them in other modules.
     
--- sim/top_vga/top_vga_tb.sv
+++ sim/top_vga/top_vga_tb.sv
@@ -28,7 +28,7 @@ module top_vga_tb;
  *  Local parameters
  */
 
-localparam CLK_PERIOD = 25;     // 40 MHz
+localparam CLK_PERIOD = 20;     // 50 MHz
 
 /**
  * Local variables and signals
